d2f506e852319e1b0c7ff326f2a9189167de2e79
mustafakba <kaba.mustafaa@gmail.com>
2022-10-28 19:57:35 +0300

    homepage tasarimi devami

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="">Deneme dosyası </div>
-    <div class="flex text-xl justify-center">Allahım sen yardım et büyüksün ya rabbim</div>
+<HomePage></HomePage>
 </template>
 
 <script>
-
+import HomePage from "@/components/HomePage";
 export default {
   name: 'App',
   components: {
+      HomePage
   }
 }
 </script>
 
 <style lang="scss">
 #app {
+    height: 100%;
+    background-color: #0A142F;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,168 @@
+<template>
+    <navbar>
+        <div class="cont flex items-center justify-evenly py-5 mr-20">
+            <div style="margin-top: 5px; margin-right: 50px" class="logo py-2.5 ">
+                <img class="cursor-pointer " src="../image/dalio.svg" alt="">
+            </div>
+            <div class="lines absolute">
+                <img src="../image/left-background-lines.svg" alt="">
+            </div>
+            <div class="eclipse absolute">
+                <img src="../image/eclipse-ball.svg" alt="">
+            </div>
+            <div class="eclipse-star absolute">
+                <img src="../image/star.svg" alt="">
+            </div>
+            <div class="menu-items">
+                <a href="#">About Us</a>
+                <a href="#">What We Do </a>
+                <a href="#">Our work</a>
+                <a href="#">Blog</a>
+                <a href="#">Say hi</a>
+            </div>
+            <div class="menu-icon cursor-pointer relative">
+                <img src="../image/menu.svg" alt="">
+                <img class="menu-star absolute" src="../image/star.svg" alt="">
+            </div>
+        </div>
+    </navbar>
+    <section class="relative mr-5" id="main">
+        <div class="title mr-20">
+            <h2 class="relative digital">Digital
+            <img class="long-line  absolute" src="../image/long-lines.svg" alt="">
+            </h2>
+            <div class="absolute world-class flex items-center justify-center ">
+                <img class="relative" src="../image/border.svg" alt="">
+                <span class="rotate-text absolute">World Class</span>
+                <img class="relative rotate-lines" src="../image/group-line.svg" alt="">
+            </div>
+            <h2 class="title agency  relative">Agency
+                <img class="absolute right-image ml-2.5" src="../image/homepage-right.svg" alt="">
+            </h2>
+        </div>
+        <div class="explain">
+            <p class="explain-text">Full-service design support that growing B2B companies need. World leading agency.</p>
+            <div class="our-project">
+                <button class="yellow-button flex items-center ">Our Projects
+                    <img style="width: 15px; height: 15px" class="ml-2.5" src="../image/turn.svg" alt="">
+                </button>
+            </div>
+        </div>
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: "HeaderComponent"
+}
+</script>
+
+<style lang="scss" scoped>
+
+.cont{
+    .menu-items{
+        a{
+            font-style: normal;
+            color: #FFF;
+            font-family: Inter,sans-serif;
+            margin-right: 50px;
+            font-size: 18px;
+            font-weight: 500;
+            line-height: 22px;
+        }
+    }
+    .lines{
+        width: 490px;
+        height: 428px;
+        top: 0;
+        left: 0;
+    }
+    .eclipse{
+        left:0;
+        top: 200px;
+        opacity: .4;
+    }
+    .eclipse-star{
+    left: 55px;
+    top: 250px;
+
+    }
+    .menu-star{
+        top: 77px;
+        right: 99px;
+        min-width: 54px;
+        min-height: 54px;
+    }
+}
+#main{
+    margin-top: 70px;
+    .title{
+        .digital{
+            margin-left: 150px;
+            .long-line{
+                width: 717px;
+                left: 595px;
+                top: 10px;
+            }
+        }
+        .agency{
+            .right-image{
+                top: 0;
+                left:945px ;
+                margin-left: 30px!important;
+            }
+        }
+        h2{
+            font-size: 150px;
+            color: #FFFFFF;
+            line-height: 181px;
+        }
+        .world-class{
+            top: 70px;
+            left: 300px;
+            color: #FFFFFF;
+            .rotate-text{
+                left: 59px;
+                transform: rotate(332deg);
+            }
+            .rotate-lines{
+                top: -90px;
+                left: -30px;
+            }
+        }
+    }
+    .explain{
+        text-align: left;
+        margin-right: auto;
+        margin-left: auto;
+        margin-top: 22px;
+        width: 555px;
+        height: 74px;
+        .explain-text{
+            font-family: Inter;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 24px;
+            line-height: 40px;
+            color:#E0E5F3 ;
+            opacity: .8;
+            margin-right:50px ;
+        }
+        .our-project{
+            display: flex;
+            flex-direction: row;
+            width: 200px;
+            margin-top: 40px;
+            .yellow-button{
+                color: #0A142F;
+                opacity: 1;
+                padding: 20px 30px;
+                border-radius: 30px;
+                background-color: #FFC93E;
+                height: 60px;
+            }
+        }
+    }
+}
+</style>
